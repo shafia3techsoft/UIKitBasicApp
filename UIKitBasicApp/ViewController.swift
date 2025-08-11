ca51eab9dd421a0bf85236208649506291053d10
shafia.3techsoft <shafia.3techsoft@gmail.com>
2025-08-11 17:07:14 +0500

    initial Commit

--- UIKitBasicApp/ViewController.swift
+++ UIKitBasicApp/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBAction func simpleButton(_ sender: UIButton) {
+        titleLabel.text = "Simple Button Clicked!"
+    }
+    
+    @IBAction func tintedButton(_ sender: UIButton) {
+        titleLabel.text = "Tinted Button Clicked!"
+    }
+    @IBOutlet weak var textField : UITextField!
+    @IBOutlet weak var hStack: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        titleLabel.text = "Yes i am in Explore View"
+        titleLabel.textColor = UIColor.green
+        hStack.translatesAutoresizingMaskIntoConstraints = false
+        hStack.layer.borderColor = UIColor.systemIndigo.cgColor
+        hStack.layer.borderWidth = 1.5
+        hStack.layer.cornerRadius = 8.0
     }
 
-
+    
 }
-
