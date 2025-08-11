64af9bd636a481d85c309dd5e97b0451b264d9e1
shafia.3techsoft <shafia.3techsoft@gmail.com>
2025-08-11 12:52:15 +0500

    Initial Commit

---
+++ UIKitBasicApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitBasicApp
+//
+//  Created by shafia on 11/08/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UIKitBasicAppTests/UIKitBasicAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitBasicAppTests.swift
+//  UIKitBasicAppTests
+//
+//  Created by shafia on 11/08/2025.
+//
+
+import Testing
+@testable import UIKitBasicApp
+
+struct UIKitBasicAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
